12d853bbc26f46a97f042f6a8a99392ae59afef2
vrunda jasani <131241676+Vrunda432@users.noreply.github.com>
2023-09-12 12:21:34 +0530

    Add files via upload

---
+++ fm tree.js.js
@@ -0,0 +1,67 @@
+ //family tree//
+
+
+ console.log("-----------family----------");
+
+ let family={
+   garndfather:del={
+      name:"babudada",
+      age:86,
+      occupation:"farmer",
+   
+   },
+   garndfather_child:["kishor","jaysukh","ramesh"],
+   kishor:kishor_del={
+      age:55,
+      occupation:"zonal-marketingmanager",
+      child:["vrunda","ishan"],
+      vehical_details:["car","bike"],
+      car:car_del={
+         company:"hundai",
+         model:"creta",
+         car_number:"gj131608"
+
+      },
+      bike:bike_del={
+         model:"bullet",
+         bike_number:"gj135543"
+      },
+      
+   },
+   jaysukh:jaysukh_del={
+      age:60,
+      occupation:"farmer",
+      child:["kirtan","mansi"],
+      vehicle_details:["car","bike"],
+      car:car_del={
+         company:"maruti",
+         model:"zen",
+         car_number:"Gj114555",
+      },
+      bike:bike_del={
+         company:"honda",
+         model:"spelnder",
+         bike_number:"Gj115655",
+      },
+   },
+      ramesh:ramesh_del={
+         age:60,
+         occupation:"job",
+         child:["kiitu","mital"],
+         vehicle_details:["car","bike"],
+         car:car_del={
+            company:"skcoda",
+            model:"zen5",
+            car_number:"Gj53344"
+         },
+         bike:bike_del={
+            company:"honda",
+            model:"spelnder",
+            bike_number:"Gj58989",
+         },
+   },
+}
+ console.log(family);
+
+
+ 
