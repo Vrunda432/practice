78d51dad85d1b2cf81e9e17cb4942a7e45f5627b
vrunda jasani <131241676+Vrunda432@users.noreply.github.com>
2023-09-12 12:19:10 +0530

    Add files via upload

---
+++ family tree.js.js
@@ -0,0 +1,15 @@
+function fun(a,b,c){
+    console.log("----->")
+    return[1,2,2]
+}
+let x=fun(1,[1,2,3,4])
+
+let fun2=function(){
+    console.log("------------->")
+
+}
+fun2()
+const fun3= (x)=>{
+    console.log("---fun3--->",x)
+}
+fun3(1000)
